043447e9124c6ca04469251a1743e65ab2882a16
VdovinKNT <146138335+VdovinKNT@users.noreply.github.com>
2025-03-24 05:08:51 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = '7728878781:AAHsQyAPeN1Ry9ycxoIp7I-sH4FFlUC34nE'
+BOT_TOKEN = "7728878781:AAHsQyAPeN1Ry9ycxoIp7I-sH4FFlUC34nE"
 MONGO_URI = "mongodb://localhost:27017/"
 DB_NAME = "task_manager"
